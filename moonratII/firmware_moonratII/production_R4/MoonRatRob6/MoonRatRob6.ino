6d6894baa6dcb43c0350eb1540d4fe1bbdf99697
Robert L. Read <read.robert@gmail.com>
2024-11-02 17:45:15 -0500

    separating time persistence, display not working

--- moonratII/firmware_moonratII/production_R4/MoonRatRob6/MoonRatRob6.ino
+++ moonratII/firmware_moonratII/production_R4/MoonRatRob6/MoonRatRob6.ino
@@ -1,24 +1,41 @@
-//Wokwi version: V10 (https://wokwi.com/projects/390933163302682625)
+//  Moonrat Control Code
+//  Copyright (C) 2024 Robert L. Read, Horacio García-Vázquez
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Define control strategy 
 // #define STRATEGY_THERMOSTAT 1
 // #define STRATEGY_PID 2
 #define STRATEGY_FUZZY 3
-
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
 #if defined(STRATEGY_FUZZY)
 #include <Fuzzy.h>
 #endif
 #if defined(STRATEGY_PID)
 #include <PID_v1.h>
 #endif
+// The thermostat function requires no code!
+
+#include <stdio.h>
+#include <math.h>
+#include <EEPROM.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "Persistence.h"
 
 // // Set controller type here
 // #define R3 //Uncomment for Arduino UNO R3
@@ -42,21 +59,42 @@ DallasTemperature sensor(&ourWire);
 // I/0 VARIABLES
 // NOTE: Rob thinks this is a TMP36
 // const int sensorPin = A0; //TMP37 SOC pin
-#define HEATER_PIN  10 //Heater (termopad) pin
+int HEATER_PIN = 10; //Heater (termopad) pin
 #define BUZZER_PIN  9 // Buzzer pin
 #define BUTTON_SELECT 5 //Button = SELECT pin
 #define BUTTON_UP 7 //Button =  UP pin
 #define BUTTON_DOWN 6 //Button = DOWN pin
 #define TRIGGER_PIN 11 // used to set a time signal for triggering oscilloscope
 
+int LOG_VERBOSE = 5;
+int LOG_DEBUG   = 4;
+int LOG_WARNING = 3;
+int LOG_MAJOR   = 2;
+int LOG_ERROR   = 1;
+int LOG_PANIC   = 0;
+
+int LOG_LEVEL   = 5;
+
 const int DEBUG_TEMP = 1;
+
+
+#define MAX_TEMPERATURE_C 42.0
+
+
+// #define USE_LOW_TEMP 1
+#ifdef USE_LOW_TEMP
+float targetTemperatureC = 30.0;  // Celcius
+#else
+float targetTemperatureC = 35.0;  // Celcius
+#endif
+
 float CurrentTemp;
 float OldErrorInput = 0.0;
 float calibration;
 
 // GRAPH VARIABLES
 const int numPoints = 60;
-float TempHistory[numPoints] = {0};
+// float TempHistory[numPoints] = {0};
 const int barWidth = SCREEN_WIDTH / numPoints;
 
 // MENU VARIABLES
@@ -141,6 +179,22 @@ FuzzySet *average = new FuzzySet(0, 5, 5, 20);
 FuzzySet *slow = new FuzzySet(0, 0, 1, 5);
 #endif
 
+//display variables
+#define WIDTH 128
+#define HEIGHT 64
+#define SPLIT 16  // location of the graphing area (vertially, down from the top)
+#define LEFT_MARGIN 9
+#define LINE_HEIGHT 9
+// #define OLED_RESET 4
+
+// graph stuff
+int center = SPLIT + ((64 - SPLIT) / 2);  // (of ex axis?)
+int graphwidth = 128 - LEFT_MARGIN;
+int xaxisleft = LEFT_MARGIN * 2;
+int xaxismid = LEFT_MARGIN + (graphwidth / 2) - 4;
+int xaxisright = 128 - 24;
+
+
 static const unsigned char PROGMEM image_data_Saraarray[] = {
     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
@@ -210,58 +264,145 @@ static const unsigned char PROGMEM image_data_Saraarray[] = {
 
 
 
-void renderDisplay() {
-    display.clearDisplay();
-      display.setTextSize(1);
-      display.setTextColor(SSD1306_WHITE);
-      display.setCursor(0, 2);
-      display.print("Temp:");
-      display.print(FilteredTemp);
-      display.println("C ");
+void showGraph(int eeindex) {
 
-    // Show the horizontal lines for maximum and minimum temperatures.
-    int yMax = map(tempMax, 0, 45, 0, 14);
-    int yMin = map(tempMin, 0, 45, 50, 0);
-    //display.drawFastHLine(0, yMax, SCREEN_WIDTH, SSD1306_WHITE);
-    display.drawLine(0, 10, SCREEN_WIDTH, 10, SSD1306_WHITE);
-    //display.drawFastHLine(0, yMin, SCREEN_WIDTH, SSD1306_WHITE);
-    display.drawLine(0, 54, SCREEN_WIDTH, 54, SSD1306_WHITE);
+  Serial.print(F("eeindex: "));
+  Serial.println(eeindex);
 
- // Display temperature graph on the rest of the screen
-    for (int i = 0; i < numPoints; i++) {
-      int x = i * barWidth;
-      int barHeight = map(TempHistory[i], 0, 50, 0, 44);
-      //display.fillRect(x, SCREEN_HEIGHT - barHeight, barWidth, barHeight-14, SSD1306_WHITE);
-      display.fillRect(x, 54 - barHeight, barWidth, barHeight, SSD1306_WHITE);
-      //display.fillRect(x, 32 , 1, 6, SSD1306_WHITE);
-    }
+  //  int graphMaxTemp = targetTemperatureC + 2; //initializing max temp
+  //  int graphMinTemp = targetTemperatureC - 2; //initializing min temp
+  //  int graphMaxTemp1 = targetTemperatureC + 20; //initializing max temp
+  //  int graphMinTemp1 = targetTemperatureC - 20; //initializing min temp
+  //  int maxtarget = targetTemperatureC + 2; //max range for target temp
+  //  int mintarget = targetTemperatureC - 2; //min range for target temp
+  //TODO find out have to shift x axis values
+  //Graph
 
-    // Show the maximum and minimum temperature label
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(74, 2); // Position adjustment for Max legend
-    display.print("Goal:");
-    display.print(tempMax);
-    display.print("C");
-    display.setCursor(45, SCREEN_HEIGHT-8);
-    display.print("Timer:");
-    display.print(hours);
-    display.print(":");
-    display.print(minutes);
-    display.print(":");
-    display.print(seconds);
+  int num_samples_drawn = min(graphwidth, eeindex);
+  // I guess these are supposed to be in hours...
+  float timeleft = 0;                                                                 //(xaxisright-xaxisleft)*FIVE_MINUTES;
+  float timemid = (((float)(num_samples_drawn) / 2) * DATA_RECORD_PERIOD) / 3600000;  //(xaxisright-xaxismid)*FIVE_MINUTES;
+  float timeright = ((float)(num_samples_drawn)*DATA_RECORD_PERIOD) / 3600000;        //xaxisright*FIVE_MINUTES;
+
+  int diff1;
+  int diff2;
+
+  // index begins at the end minus graph width
+  int startIndex = (eeindex > graphwidth) ? eeindex - graphwidth : 0;
+  //plots 119 points fron the EEPROM Contents at a period of 5 minutes
+  // This logic should render the last 119 samples, I suppose
+  float maxTempToGraph = -1.0;
+  float minTempToGraph = 5000.0;
+  for (int i = startIndex; i < eeindex; i++) {
+    float currentTemp = readIndex(i);
+    minTempToGraph = min(minTempToGraph, currentTemp);
+    maxTempToGraph = max(maxTempToGraph, currentTemp);
+  }
+
+  int graphMaxTemp = ceil(maxTempToGraph);
+  int graphMinTemp = floor(minTempToGraph);
+
+  Serial.print(F("graph midpoint ")); 
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print(F("# sam:"));
+  display.println(eeindex);
+
+  display.setTextSize(1);
+  display.setCursor(1, SPLIT);
+  display.println(graphMaxTemp);
+  display.setCursor(1, 56);
+  display.println(graphMinTemp);
+  display.setCursor(LEFT_MARGIN * 2, 56);
+  display.println(timeleft);
+  display.setCursor(xaxismid, 56);
+  display.println(timemid);
+  display.setCursor(xaxisright, 56);
+  display.println(timeright);
+
+  // Pixels per degree
+  float scale = (64.0 - SPLIT) / (float)(graphMaxTemp - graphMinTemp);  //creates scale y axis pixels
+
+  float meanTemp = (((float)graphMaxTemp + (float)graphMinTemp) / 2.0);
+  float middle = meanTemp - (float)graphMinTemp;
+  int j = 0;
+
+  Serial.println(F("start, eeindex"));
+  Serial.println(startIndex);
+  Serial.println(eeindex);
+  for (int i = startIndex; i < eeindex; i++) {
+    float currentTemp = readIndex(i);
+    float ypos = (currentTemp - graphMinTemp);
+
+    int x = LEFT_MARGIN + j;
+    int y = (64 - (scale * ypos));
+    int midy = (64 - (scale * middle));
+    display.drawPixel(x, y, SSD1306_WHITE);  //plots temparature
+    display.drawPixel(x, midy, SSD1306_WHITE);
+    j++;
+  }  
+
+  display.display();
+  Serial.println(F("display done"));
+}
+
+// void renderDisplay() {
+//     Serial.println("begin render-display");
+//     display.clearDisplay();
+//         display.display();
+//     Serial.println("begin render-display");
+//       display.setTextSize(1);
+//       display.setTextColor(SSD1306_WHITE);
+//       display.setCursor(0, 2);
+//       display.print("Temp:");
+//       display.print(FilteredTemp);
+//       display.println("C ");
+
+//     // Show the horizontal lines for maximum and minimum temperatures.
+//     int yMax = map(tempMax, 0, 45, 0, 14);
+//     int yMin = map(tempMin, 0, 45, 50, 0);
+//     //display.drawFastHLine(0, yMax, SCREEN_WIDTH, SSD1306_WHITE);
+//     display.drawLine(0, 10, SCREEN_WIDTH, 10, SSD1306_WHITE);
+//     //display.drawFastHLine(0, yMin, SCREEN_WIDTH, SSD1306_WHITE);
+//     display.drawLine(0, 54, SCREEN_WIDTH, 54, SSD1306_WHITE);
+
+//  // Display temperature graph on the rest of the screen
+//     // for (int i = 0; i < numPoints; i++) {
+//     //   int x = i * barWidth;
+//     //   int barHeight = map(TempHistory[i], 0, 50, 0, 44);
+//     //   //display.fillRect(x, SCREEN_HEIGHT - barHeight, barWidth, barHeight-14, SSD1306_WHITE);
+//     //   display.fillRect(x, 54 - barHeight, barWidth, barHeight, SSD1306_WHITE);
+//     //   //display.fillRect(x, 32 , 1, 6, SSD1306_WHITE);
+//     // }
+
+//     // Show the maximum and minimum temperature label
+//     display.setTextSize(1);
+//     display.setTextColor(SSD1306_WHITE);
+//     display.setCursor(74, 2); // Position adjustment for Max legend
+//     display.print("Goal:");
+//     display.print(tempMax);
+//     display.print("C");
+//     display.setCursor(45, SCREEN_HEIGHT-8);
+//     display.print("Timer:");
+//     display.print(hours);
+//     display.print(":");
+//     display.print(minutes);
+//     display.print(":");
+//     display.print(seconds);
    
 
-    // get entrances
-    float ErrorInput = FilteredTemp-tempMax;
-    float DiffErrorInput = ErrorInput - OldErrorInput;
-    OldErrorInput = ErrorInput;
+//     // get entrances
+//     float ErrorInput = FilteredTemp-tempMax;
+//     float DiffErrorInput = ErrorInput - OldErrorInput;
+//     OldErrorInput = ErrorInput;
 
-    display.setCursor(0, SCREEN_HEIGHT-8); // Position adjustment for Min legend
-    display.print("PWM:");
- //   display.print(int(round(OutputFreq)));
-    display.display();
-}
+//     display.setCursor(0, SCREEN_HEIGHT-8); // Position adjustment for Min legend
+//     display.print("PWM:");
+//  //   display.print(int(round(OutputFreq)));
+//     Serial.println("done with render-display");
+//     display.display();
+// }
 
 
 void displayTempMenu() {
@@ -482,6 +623,21 @@ void setup() {
   minutes = 0;
   hours = 0;
 
+  incubationON();
+
+  float storedTargetTemp = getTargetTemp();
+  if (storedTargetTemp > MAX_TEMPERATURE_C) {
+    setTargetTemp(MAX_TEMPERATURE_C);
+    storedTargetTemp = MAX_TEMPERATURE_C;
+  }
+  if (storedTargetTemp < 25.0) {
+    setTargetTemp(35.0);
+    Serial.println(F("target temp to low, setting to 35!"));
+    storedTargetTemp = 35.0;
+  }
+  targetTemperatureC = storedTargetTemp;
+
+
   // This is insufficient to read the DS18B20 temperature sensor
   pinMode(A0, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -490,10 +646,6 @@ void setup() {
   analogWrite(BUZZER_PIN,0);
   delay(100);
   Serial.begin(BAUD_RATE);
-  while (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("Error al iniciar el OLED"));
-  }
-  Serial.println("OLED succesful");
 
   sensor.begin(); //* Init sensor
 
@@ -567,6 +719,8 @@ void setup() {
   displayTimeMenu();
   checkTimeButtons();
   setMaxTemp();
+
+
   Serial.println("Setup Done");
 }
 int renderDisplay_bool = 0;
@@ -608,6 +762,7 @@ int thermostatPWM() {
 }
 #endif
 
+
 int exit_flag = 0;
 void checkTimeExpired() {
 
@@ -625,11 +780,31 @@ void checkTimeExpired() {
   }
 }
 void loop() {
-  Serial.print("in loop!");
   sensor.requestTemperatures();
   CurrentTemp = sensor.getTempCByIndex(0);
   Serial.println("CurrentTemp: ");
   Serial.println(CurrentTemp);
+
+  boolean dumpdata = false;
+  boolean reset = false;
+  //read keyboard entries from the serial monitor
+  char T;
+  if (Serial.available()) {
+    T = Serial.read();  //getting string input in varaible "T"
+
+    Serial.print(F("T ="));
+    Serial.println(T);
+    dumpdata = (T == 'x');
+    reset = (T == 'r');
+  } 
+  if (dumpdata) {
+    Serial.println(F("dump Data"));
+    dumpData();
+  }
+  if (reset) {
+    Serial.println(F("reset ROM INDEX"));
+    rom_reset();
+  }
 
 #if defined(KALMAN)
   FilteredTemp = kalmanFilter(CurrentTemp,FilteredTemp);
@@ -638,11 +813,10 @@ void loop() {
 #endif
 
   if (exit_flag) {
-    Serial.println("INCUBATION NONE.");
+    Serial.println("INCUBATION DONE.");
     return;
   }
 
-  delay(1000);
   selectedOption = 0;
 
   if ((digitalRead(BUTTON_UP) == HIGH) && (digitalRead(BUTTON_DOWN) == HIGH) && (digitalRead(BUTTON_SELECT) == HIGH))
@@ -653,22 +827,23 @@ void loop() {
   }
 
   checkTimeExpired();
-    
-  delay(1000);
   timeNow = (millis()/1000); // the number of milliseconds that have passed since boot
   seconds = timeNow;
 
     // assert(secondsToUpdateDisplay > secondsToUpdateTemp);
   if((seconds - secondsSinceTempUpdate) >= secondsToUpdateTemp) // This occurs one per second....
   {
-// This should be replaced with a RingBuffer style implementation....
-    for (int i = 0; i < numPoints - 1; i++)
-    {
-      TempHistory[i] = TempHistory[i + 1];
-    }
-    TempHistory[numPoints - 1] = FilteredTemp;
+      Serial.println("Updating Temp");
+// // This should be replaced with a RingBuffer style implementation....
+//     for (int i = 0; i < numPoints - 1; i++)
+//     {
+//       TempHistory[i] = TempHistory[i + 1];
+//     }
+//     TempHistory[numPoints - 1] = FilteredTemp;
     renderDisplay_bool = (seconds - secondsLastDisplay) > secondsToUpdateDisplay;
     if (renderDisplay_bool) {
+
+      Serial.println("Updating Display");
       float outputPWM;
 #if defined(STRATEGY_FUZZY)
       outputPWM = fuzzyPWM();
@@ -679,7 +854,9 @@ void loop() {
 #if defined(STRATEGY_THERMOSTAT)
       outputPWM = thermostatPWM();
 #endif
-      analogWrite(HEATER_PIN, int(round(outputPWM)));
+
+    setHeatPWM(int(round(outputPWM)));  
+//     analogWrite(HEATER_PIN, int(round(outputPWM)));
       Serial.print("Heater PWM: ");
       Serial.print((float) outputPWM * 100.0 / 256.0);
       Serial.println("%");
@@ -688,7 +865,11 @@ void loop() {
       display.print(int(round(outputPWM)));
       display.display();
       Serial.print("renderDisplay: ");
-      renderDisplay();
+
+      uint16_t index = getIndex();
+      showGraph(index);
+
+ //     renderDisplay();
       secondsLastDisplay = seconds;
     } else {
     }
